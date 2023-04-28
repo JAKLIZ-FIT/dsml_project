5507efb977f325f9128bf3db057582ffbc363b2e
Jiří Žilka <56739240+JAKLIZ-FIT@users.noreply.github.com>
2023-04-28 16:16:31 +0200

    moved data prep code to dataPrep file

--- src/corrMatrix.py
+++ src/corrMatrix.py
@@ -1,16 +1,15 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-data = pd.read_csv("manufacturing.csv")
-le = LabelEncoder()
-data['ionizationclas'] = le.fit_transform(data['ionizationclass'])
-data['FluxCompensation'] = le.fit_transform(data['FluxCompensation'])
-data['multideminsionality']= le.fit_transform(data['multideminsionality'])
-data['error'] = le.fit_transform(data['error'])
-data['error_type'] = le.fit_transform(data['error_type'])
+from dataPrep import *
 
-data.drop("id", axis = 1,inplace=True)
+def showCorrMatrix(df):
+    corrMatrix = df.corr()
+    print(corrMatrix)
+    return corrMatrix
 
-corrMatrix = data.corr()
-print(corrMatrix)
-
+if __name__ == '__main__':
+    df = loadData()
+    print(df)
+    df = prepareData(df)
+    cm = showCorrMatrix(df)
