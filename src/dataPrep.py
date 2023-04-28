51754f5fb72132afd69c21de9ca263f958db9fec
Jiří Žilka <56739240+JAKLIZ-FIT@users.noreply.github.com>
2023-04-28 15:32:07 +0200

    Update dataPrep.py

--- src/dataPrep.py
+++ src/dataPrep.py
@@ -2,6 +2,22 @@
 """
 Created on Fri Apr 28 14:19:30 2023
 
-@author: jzilk
+@author: jiri zilka
 """
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
+
+def showCorrMatrix():
+    pass 
+
+def prepareData(df):
+    
+
+if __name__ == '__main__':
+    df = pd.read_csv('../data/manufacturing.csv')
+    
+    df = prepareData(df)
+    
+    showCorrMatrix(df)
+    
