7aed3060cb203cb6f1dfb6a96028e3f1f68a0f26
Jiří Žilka <56739240+JAKLIZ-FIT@users.noreply.github.com>
2023-04-28 16:14:06 +0200

    added Label encoding to data preparation

--- src/dataPrep.py
+++ src/dataPrep.py
@@ -5,19 +5,35 @@ Created on Fri Apr 28 14:19:30 2023
 @author: jiri zilka
 """
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 
+nanColumns = ['ionizationclass','FluxCompensation','multideminsionality',
+               'error','error_type']
 
-def showCorrMatrix():
-    pass 
+def loadData(path='../data/manufacturing.csv'):
+    return pd.read_csv(path)
 
-def prepareData(df):
+def prepareData(df,encoding='basic'):
+    df.drop("id", axis = 1,inplace=True)
+    if encoding == 'basic':
+        le = LabelEncoder()
+        for column in nanColumns:
+            df[column] = le.fit_transform(df[column])
+    elif encoding == 'onehot':
+        pass # TODO
+    else:
+        pass
+    return df
     
+def removeOutliers(df):
+    #TODO
+    pass
+
+def normalize(df):
+    #TODO
+    pass
 
 if __name__ == '__main__':
-    df = pd.read_csv('../data/manufacturing.csv')
-    
+    df = loadData()
     df = prepareData(df)
-    
-    showCorrMatrix(df)
     
