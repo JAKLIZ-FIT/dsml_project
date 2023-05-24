58edca609bce3f5ec4a8fc597c5c8faee8fa1f8d
Jiří Žilka <56739240+JAKLIZ-FIT@users.noreply.github.com>
2023-05-24 16:24:16 +0200

    added normalization, outlier removal

--- src/dataPrep.py
+++ src/dataPrep.py
@@ -5,7 +5,7 @@ Created on Fri Apr 28 14:19:30 2023
 @author: jiri zilka
 """
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, normalize
 
 nanColumns = ['ionizationclass','FluxCompensation','multideminsionality',
                'error','error_type']
@@ -15,6 +15,7 @@ def loadData(path='../data/manufacturing.csv'):
 
 def prepareData(df,encoding='basic'):
     df.drop("id", axis = 1,inplace=True)
+    df = df[df['width'] != 10000000000]
     if encoding == 'basic':
         le = LabelEncoder()
         for column in nanColumns:
@@ -24,16 +25,21 @@ def prepareData(df,encoding='basic'):
     else:
         pass
     return df
+
+def normalizeData(df,n_axis=0):
+    return pd.DataFrame(normalize(df,axis=n_axis),columns=df.columns)    
     
+
 def removeOutliers(df):
     #TODO
     pass
 
-def normalize(df):
-    #TODO
-    pass
+# useless, it takes one line anyway
+def splitData():
+    return ()
 
 if __name__ == '__main__':
     df = loadData()
     df = prepareData(df)
+    df = normalizeData(df)
     
