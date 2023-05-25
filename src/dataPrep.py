566d4271a281eb1ba338ac8188302bf58cc21307
Jiří Žilka <56739240+JAKLIZ-FIT@users.noreply.github.com>
2023-05-25 09:08:52 +0200

    Update dataPrep.py

--- src/dataPrep.py
+++ src/dataPrep.py
@@ -7,6 +7,7 @@ Created on Fri Apr 28 14:19:30 2023
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder, normalize
 
+# columns that need to be encoded
 nanColumns = ['ionizationclass','FluxCompensation','multideminsionality',
                'error','error_type']
 
@@ -34,17 +35,27 @@ def prepareData(df,encoding='basic',encColumns=[]):
         pass
     return df
 
-def normalizeData(df,n_axis=0):
-    return pd.DataFrame(normalize(df,axis=n_axis),columns=df.columns)    
-    
+def normalizeData(df,n_axis=0, type='norm'):
+    if type == 'norm':
+        return pd.DataFrame(normalize(df,axis=n_axis),columns=df.columns)    
+    elif type == 'minMax':
+        from sklearn import preprocessing
+        min_max_scaler = preprocessing.MinMaxScaler()
+        df = pd.DataFrame(min_max_scaler.fit_transform(df),columns=df.columns)
+    else:
+        return df
 
-def removeOutliers(df):
-    df = df[df['width'] != 10000000000]
-    pass
+def removeOutliers(df, useMedian=False):
+    if useMedian:
+        df.loc[df['width'] == 10000000000,'width'] = df.width.median()
+    else: 
+        df = df[df['width'] != 10000000000]
+    return df
 
-# useless, it takes one line anyway
-def splitData():
-    return ()
+# split data into train set and test set 
+def splitData(X,Y):
+    from sklearn.model_selection import train_test_split
+    return train_test_split(X, Y, test_size=0.25,random_state=42)
 
 if __name__ == '__main__':
     df = loadData()
